0b29a6b3fa1e7fd4c6ea68f5d3035ea280994cec
Franklin Shera <fshera96@gmail.com>
2023-07-01 18:43:34 +0300

    fix: translation functions

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,18 +1,50 @@
-import {
-  translations,
-  type Languages,
-  type Translation,
-  defaultLang,
-} from "./config";
+import { translations, type Languages, defaultLang, TPath } from "./config";
 
+/**
+ * Retrieves the language code from the URL path.
+ */
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split("/");
   if (lang in translations) return lang as Languages;
   return defaultLang;
 }
 
+/**
+ * Returns the translation value for a given translation key using the default language.
+ */
+function translationFallback(keys: string[], defaultFallbackKeys: string) {
+  let value: any = translations;
+
+  for (let key of [defaultLang, ...keys]) {
+    if (value && typeof value === "object" && key in value) {
+      value = value[key as keyof typeof value];
+    } else {
+      value = `__${defaultFallbackKeys}__`;
+    }
+  }
+
+  return value as string;
+}
+
+/**
+ * Returns a translation function for the specified language.
+ */
 export function useTranslations(lang: Languages) {
-  return function t(key: keyof Translation[Languages]) {
-    return translations[lang][key] || translations[defaultLang][key];
+  /**
+   * Translates the given translation key.
+   */
+  return function t(translationKey: TPath) {
+    const keys = translationKey.split(".");
+    let value: any = translations;
+
+    for (let key of [lang, ...keys]) {
+      if (value && typeof value === "object" && key in value) {
+        value = value[key as keyof typeof value];
+      } else {
+        value = translationFallback(keys, `${lang}.${translationKey}`);
+      }
+    }
+
+    return value as string;
   };
 }
